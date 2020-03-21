8e4e69f6ddd539ec237bfdc2d6043bbe802bb339
zivza94 <zivza94@gmail.com>
2020-03-21 22:01:02 +0200

    fix app

--- FlightSimulatorView/Properties/AssemblyInfo.cs
+++ FlightSimulatorView/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlightSimulatorView")]
+[assembly: AssemblyTitle("FlightSimulatorApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlightSimulatorView")]
+[assembly: AssemblyProduct("FlightSimulatorApp")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
