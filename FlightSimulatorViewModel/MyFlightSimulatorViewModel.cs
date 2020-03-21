2affc01d5dd9097ff408eb5381b895fc2f52ca9c
zivza94 <zivza94@gmail.com>
2020-03-21 21:12:20 +0200

    fix names

--- FlightSimulatorViewModel/MyFlightSimulatorViewModel.cs
+++ FlightSimulatorViewModel/MyFlightSimulatorViewModel.cs
@@ -16,7 +16,6 @@ namespace FlightSimulatorViewModel
         public event PropertyChangedEventHandler PropertyChanged;
         private double _aileron;
         private double _throttle;
-
         public MyFlightSimulatorViewModel(IFlightSimulatorModel model)
         {
             _model = model;
@@ -35,52 +34,42 @@ namespace FlightSimulatorViewModel
         {
             get { return _model.Heading; }
         }
-
         public double VM_VerSpeed
         {
             get { return _model.VerSpeed; }
         }
-
         public double VM_GroundSpeed
         {
             get { return _model.GroundSpeed; }
         }
-
         public double VM_AirSpeed
         {
             get { return _model.AirSpeed; }
         }
-
         public double VM_GpsAltitude
         {
             get { return _model.GpsAltitude; }
         }
-
         public double VM_Roll
         {
             get { return _model.Roll; }
         }
-
         public double VM_Pitch
         {
             get { return _model.Pitch; }
         }
-
         public double VM_AltAltitude
         {
             get { return _model.AltAltitude; }
         }
-
         public double VM_Latitude
         {
             get { return _model.Latitude; }
         }
-
         public double VM_Longitude
         {
             get { return _model.Longitude; }
         }
-
         public double VM_Throttle
         {
             get { return _throttle;}
@@ -90,7 +79,6 @@ namespace FlightSimulatorViewModel
                 _model.SetThrottle(_throttle);
             }
         }
-
         public double VM_Aileron
         {
             get { return _aileron;}
@@ -100,11 +88,9 @@ namespace FlightSimulatorViewModel
                 _model.SetAileron(_aileron);
             }
         }
-
         public void SetRudderAndElevator(double rudder, double elevator)
         {
             _model.SetRudderAndElevator(rudder,elevator);
         }
-
     }
 }
