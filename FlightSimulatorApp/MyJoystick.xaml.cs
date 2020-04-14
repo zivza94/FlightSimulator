eeaf9e8a5b9dc84146e7498cb5f8b8dd3eadb9b0
Liza Yemini <liza.gilkarov@live.biu.ac.il>
2020-04-14 12:30:41 +0300

    Fixed rudder and elevator values of joystick, fixed the movement of the joystick

--- FlightSimulatorApp/MyJoystick.xaml.cs
+++ FlightSimulatorApp/MyJoystick.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,10 +19,28 @@ namespace FlightSimulatorApp
     /// </summary>
     public partial class MyJoystick : UserControl
     {
+        private Point knobCenter;
+        private bool mousePressed, xMax, yMax;
+        //members for the Animation
+        protected Storyboard centerKnobStoryBoard;
+        private DoubleAnimation x, y;
+        private double x2, y2, rudder, elevator;
+        
+        private FlightSimulatorModel.FlightSimulatorControlVM controlVM;
         public MyJoystick()
         {
             InitializeComponent();
             DataContext = (Application.Current as App).controlVM;
+            controlVM = DataContext as FlightSimulatorModel.FlightSimulatorControlVM;
+            mousePressed = false;
+            x2 = 0;
+            y2 = 0;
+            knobCenter = new Point(joystick.Base.Width / 2, joystick.Base.Height / 2);
+            centerKnobStoryBoard = joystick.Knob.Resources["CenterKnob"] as Storyboard;
+            x = centerKnobStoryBoard.Children[0] as DoubleAnimation;
+            y = centerKnobStoryBoard.Children[1] as DoubleAnimation;
+            x.From = 0;
+            y.From = 0;
         }
 
         private void Aileron_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -31,6 +50,99 @@ namespace FlightSimulatorApp
         private void Throttle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
+        }
+
+        private void joystick_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousePressed)
+            {
+                x2 = e.GetPosition(joystick.Base).X;
+                y2 = e.GetPosition(joystick.Base).Y;
+                double bound = Math.Sqrt(Math.Pow(x2 - knobCenter.X, 2) + Math.Pow(y2 - knobCenter.Y, 2));
+                //check if the knobBase is in Bound
+                if (bound > (joystick.Base.Width / 4) - (joystick.KnobBase.Width / 2))
+                {
+                    xMax = true;
+                } else
+                {
+                    xMax = false;
+                }
+                if (bound > (joystick.Base.Height / 4) - (joystick.KnobBase.Height / 2))
+                {
+                    yMax = true;
+                }
+                else
+                {
+                    yMax = false;
+                }
+                if (xMax && yMax)
+                {
+                    return;
+                }
+                if (xMax == false)
+                {
+                    x.To = x2 - knobCenter.X;
+                }
+                if (yMax == false)
+                {
+                    y.To = y2 - knobCenter.Y;
+                }
+                //start the animation          
+                centerKnobStoryBoard.Begin();
+                UpdateRudderAndElevator((double)x.To, (double)y.To);
+                if (xMax == false)
+                {
+                    x.From = x.To;
+                }
+                if (yMax == false)
+                {
+                    y.From = y.To;
+                }
+            }
+        }
+
+        private void Joystick_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mousePressed = true;
+            joystick.Knob.CaptureMouse();
+        }
+
+        private void joystick_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            centerKnobStoryBoard = joystick.Knob.Resources["CenterKnob"] as Storyboard;
+            mousePressed = false;
+            joystick.Knob.ReleaseMouseCapture();
+            //Return the animation to the middle
+            x.To = 0;
+            y.To = 0;
+            //start the animation
+            centerKnobStoryBoard.Begin();
+            controlVM.SetRudderAndElevator(0, 0);
+            x.From = x.To;
+            y.From = y.To;
+        }
+
+        private void UpdateRudderAndElevator(double x, double y)
+        {
+            rudder = x / (joystick.KnobBase.Width / 2.8);
+            elevator = y / (joystick.KnobBase.Height / 2.8);
+            if (rudder > 1)
+            {
+                rudder = 1;
+            }
+            if (rudder < -1)
+            {
+                rudder = -1;
+            }
+            if (elevator > 1)
+            {
+                elevator = 1;
+            }
+            if (elevator < -1)
+            {
+                elevator = -1;
+            }
+            controlVM.SetRudderAndElevator(rudder, elevator);
         }
     }
 }
--- FlightSimulatorApp/controls/Joystick.xaml.cs
+++ FlightSimulatorApp/controls/Joystick.xaml.cs
@@ -19,24 +19,9 @@ namespace FlightSimulatorApp.controls
     /// </summary>
     public partial class Joystick : UserControl
     {
-        private Point knobCenter;
-        private bool mousePressed;
-        //members for the Animation
-        private Storyboard centerKnobStoryBoard;
-        private DoubleAnimation x, y;
-        private double x2, y2;
         public Joystick()
         {
             InitializeComponent();
-            mousePressed = false;
-            x2 = 0;
-            y2 = 0;
-            knobCenter = new Point(Base.Width / 2, Base.Height / 2);
-            centerKnobStoryBoard = Knob.Resources["CenterKnob"] as Storyboard;
-            x = centerKnobStoryBoard.Children[0] as DoubleAnimation;
-            y = centerKnobStoryBoard.Children[1] as DoubleAnimation;
-            x.From = 0;
-            y.From = 0;
         }
 
         public double xConverted
@@ -61,47 +46,5 @@ namespace FlightSimulatorApp.controls
 
         }
 
-        private void Knob_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (mousePressed)
-            {
-                x2 = e.GetPosition(Base).X;
-                y2 = e.GetPosition(Base).Y;
-                //cheak if the knobBase is in Bound
-                double bound = Math.Sqrt(Math.Pow(x2 - knobCenter.X, 2) + Math.Pow(y2 - knobCenter.Y, 2));
-                if (bound > (this.Base.Width / 2) - (KnobBase.Width / 2) || bound > (this.Base.Height / 2) - (KnobBase.Height / 2))
-                {
-                    return;
-                }
-                else
-                {
-                    x.To = x2 - knobCenter.X;
-                    y.To = y2 - knobCenter.Y;
-                    //start the animation
-                    centerKnobStoryBoard.Begin();
-                    x.From = x.To;
-                    y.From = y.To;
-                }
-            }
-        }
-
-        private void Knob_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            mousePressed = true;
-            Knob.CaptureMouse();
-        }
-
-        private void Knob_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            mousePressed = false;
-            Knob.ReleaseMouseCapture();
-            //Return the animation to the middle
-            x.To = 0;
-            y.To = 0;
-            //start the animation
-            centerKnobStoryBoard.Begin();
-            x.From = x.To;
-            y.From = y.To;
-        }
     }
 }
